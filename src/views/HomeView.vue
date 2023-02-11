6a37a9f49fb28ca21c13d996c4868c0b69305094
Tyrannas <someonefefe@gmail.com>
2023-02-11 14:06:59 +0100

    fix view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,27 +5,15 @@ import ImageList from '../components/ImageList.vue'
 
 <template>
   <main>
-      <div class="row">
-        <div class="col-3 bg-dark">
-          Ici une barre de menu
+      <div class="container-fluid h-100">
+        <div class="row justify-content-center h-100">
+          <div class="col-3 bg-dark">
+            Ici une barre de menu
+          </div>
+          <div class="col-9">
+            <ImageList />
+          </div>
         </div>
-        <div class="col-9">
-          <ul class="nav nav-tabs">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-        </div>
-        <ImageList />
       </div>
 
   </main>
