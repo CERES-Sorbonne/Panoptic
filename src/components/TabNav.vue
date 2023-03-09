a798650e17b4b28cc0ffcecd9562e08be045014c
david <godicked@gmail.com>
2023-03-10 00:34:00 +0100

    save tab system + other ui components

---
+++ src/components/TabNav.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { onMounted } from 'vue';
+import { fakeStore } from '../fakestore';
+
+const store = fakeStore
+
+// const props = defineProps({ 
+//     selected: String
+//  })
+
+//  const emits = defineEmits(['update:selected'])
+
+
+function select(tab) {
+    store.selectTab(tab)
+}
+
+function newTabName() {
+    let counter = store.tabs.length + 1
+    let nameFnc = () => 'Tab' + counter
+    let name = nameFnc()
+    while(store.tabs.includes(name)) {
+        counter++
+        name = nameFnc()
+    }
+    return name
+}
+
+function addTab(event) {
+    let tabName = newTabName()
+    store.addTab(tabName)
+    store.selectedTabName = tabName
+}
+
+onMounted(() => {
+
+})
+
+</script>
+
+<template>
+    <nav>
+        <div class="nav nav-tabs">
+            <button v-for="tab in store.tabs" class="nav-link" :class="(tab.name == store.selectedTabName ? ' active' : '')" @click="select(tab.name)"><span class="h4">{{ tab.name }}</span></button>
+            <button class="nav-link no-border" @click="addTab"><span class="h3 bi bi-plus"></span></button>
+        </div>
+    </nav>
+</template>
+
+<style>
+.no-border{
+   border: none !important;
+}
+
+</style>
--- src/views/PanopticView.vue
+++ src/views/PanopticView.vue
@@ -1,110 +1,69 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import ImageGroup from '../components/ImageGroup.vue';
 import ExpandOption from '../components/Menu/ExpandOption.vue';
 import MainOption from '../components/Menu/MainOption.vue';
 import Property from '../components/Menu/Property.vue';
+import TabContent from '../components/TabContent.vue';
+import TabNav from '../components/TabNav.vue';
 import TagTree from '../components/TagTree/TagTree.vue';
+import { fakeStore } from '../fakestore';
 
 const data = reactive({
     selected: [],
     tags: [{name: 'test', children: [{name: 'test1'}, {name: 'test2', children: [{name: 'test34'}]}]}, {name: 'deep', children: [{name: 'egg'}, {name: 'other'}]}]
 })
 
+const selectedTab = ref('')
 
-const groups = reactive([
-    {
-        name: "Group1",
-        groups: [
-            {name: 'SubGroup11', images: 15},
-            {name: 'SubGroup12', images: 12}
-        ]
-    },
-    {
-        name: "Group2",
-        groups: [
-            {name: 'SubGroup21', images: 4},
-            {name: 'SubGroup22', images: 7}
-        ]
-    },
-])
+const store = fakeStore
 
 </script>
 
 <template>
     <div class=" ms-5 me-5">
         <div class="row">
-            <div class="col-3 bg-secondary text-white" style="height: 55px;">
-                <!-- <div class="row"> -->
+            <div class="col-3 bg-warning text-white p-0">
+                <div class="bg-secondary" style="height: 55px;">
                     <h1 class="text-center">Panoptic</h1>
-                <!-- </div> -->
-            </div>
-            <div class="col bg-info">
-                <div class="d-flex align-items-center" style="height: 55px;">
-                    <ul class="nav d-flex align-items-center">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Tab1</a>
+                </div>
+
+                <div>
+                    <ul class="list-group mt-2">
+                        <li class="list-group-item mb-2">
+                            <ExpandOption>
+                                <template #name>Folder</template>
+                                <template #content>
+                                    <ul class="list-group">
+                                        <li class="list-group-item">/images/vacances</li>
+                                        <li class="list-group-item">/theses/collecte</li>
+                                        <li class="list-group-item">/racist/lepen</li>
+                                    </ul>
+                                </template>
+                            </ExpandOption>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tab2</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tab3</a>
+                        <li class="list-group-item">
+                            <MainOption name="Properties">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Category</li>
+                                    <li class="list-group-item"><span class="bi bi-caret-right"></span> Name</li>
+                                    <li class="list-group-item">
+                                        <Property :name="'Category2'" type="tag-tree" :data="data" />
+                                    </li>
+                                </ul>
+                            </MainOption>
                         </li>
                     </ul>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-3 bg-warning">
-                <ul class="list-group mt-2">
-                    <li class="list-group-item mb-2">
-                        <ExpandOption>
-                            <template #name>Folder</template>
-                            <template #content>
-                                <ul class="list-group">
-                                    <li class="list-group-item">/images/vacances</li>
-                                    <li class="list-group-item">/theses/collecte</li>
-                                    <li class="list-group-item">/racist/lepen</li>
-                                </ul>
-                            </template>
-                        </ExpandOption>
-                    </li>
-                    <li class="list-group-item">
-                        <MainOption name="Properties">
-                            <ul class="list-group">
-                                <li class="list-group-item">Category</li>
-                                <li class="list-group-item"><span class="bi bi-caret-right"></span> Name</li>
-                                <li class="list-group-item">
-                                    <Property :name="'Category2'" type="tag-tree" :data="data" />
-                                </li>
-                            </ul>
-                        </MainOption>
-                    </li>
-                </ul>
             </div>
             <div class="col">
-                <div class="input-group mt-1">
-                    <div class="input-group-text">Display</div>
-                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Grid</button>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="#">Graph</a></li>
-                        <li><a class="dropdown-item" href="#">LA VERITÉ SUR MARCRON</a></li>
-                    </ul>
-                    <div class="input-group-text ms-1">Filters</div>
-                    <div class="input-group-text bg-white">Filter 1 <i class="bi bi-x-circle ms-2 text-danger"></i></div>
-                    <div class="input-group-text bg-white">Filter 2 <i class="bi bi-x-circle ms-2 text-danger"></i></div>
-                    <div class="input-group-text bg-white">Filter 3 <i class="bi bi-x-circle ms-2 text-danger"></i></div>
-                    <button class="btn btn-outline-secondary">+</button>
-
-                    <div class="input-group-text ms-1">Group By</div>
-                    <div class="input-group-text bg-white">Name <i class="bi bi-x-circle ms-2 text-danger"></i></div>
-                    <div class="input-group-text bg-white">Tags <i class="bi bi-x-circle ms-2 text-danger"></i></div>
-                    <button class="btn btn-outline-secondary">+</button>
+                <div style="min-height: 55px;">
+                    <TabNav v-model:selected="selectedTab"/>
                 </div>
-                <div class="mt-4">
-                    <ImageGroup :leftAlign="true" v-for="group in groups" :group="group"/>
+                <div v-if="store.selectedTabName">
+                    <TabContent/>
                 </div>
+                
             </div>
         </div>
     </div>
