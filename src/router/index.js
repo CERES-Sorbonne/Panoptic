fb4df4a44c94284f60448e7cddab8f421a2a5f0c
david <godicked@gmail.com>
2023-03-10 00:35:48 +0100

    set PanopticView on /test path

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/test',
+      name: 'home',
       component: PanopticView
     }
   ]
