9ca3cd77639997272d9c3ae6a6b16c6b561cedc3
Tyrannas <someonefefe@gmail.com>
2023-03-13 18:05:22 +0100

    reworking store

---
+++ src/store.ts
@@ -0,0 +1,76 @@
+import { ComputedRef } from 'vue'
+import { computed, reactive } from 'vue'
+import { apiGetImages, apiGetProperties, apiGetTags, apiAddTag, SERVER_PREFIX, apiAddProperty } from './utils/api'
+import { PropertyType, Tag, Tags, TagsTree, Property, GlobalStore, Properties, Images, PropsTree } from './utils/models'
+
+export const globalStore:GlobalStore = reactive({
+  images: {} as Images,
+  tags: {} as Tags,
+  properties: {} as Properties,
+  
+  imageList: computed(() => {
+    return Object.keys(globalStore.images).map(sha1 => {
+      return {url: SERVER_PREFIX + globalStore.images[sha1].url, imageName: sha1}
+  })}),
+
+  tagTrees: computed(() => {
+    const tree:PropsTree = {}
+    Object.entries(globalStore.tags).forEach(([propId, tags]) => {tree[parseInt(propId)] = getPropertyTree(tags)})
+    return tree 
+  }),
+
+  async fetchAllData() {
+    this.images = await apiGetImages()
+    this.tags = await apiGetTags()
+    this.properties = await apiGetProperties()
+  },
+
+  async addTag(propertyId: number, tagValue:string, parentId?:number, color?:string): Promise<void>{
+    const newTag: Tag = await apiAddTag(propertyId, tagValue, color, parentId)
+    this.tags[propertyId][newTag.id] = newTag
+  },
+
+  async addProperty(name: string, type: PropertyType){
+    const newProperty:Property = await apiAddProperty(name, type)
+    this.properties[newProperty.id] = newProperty
+  },
+
+  async addPropertyToImage(){
+
+  },
+
+  async updateTag(){
+
+  },
+
+  async updateProperty(){
+
+  },
+
+  async updateImageProperty(){
+
+  },
+
+  async addFolder(){
+
+  }
+
+})
+
+function getPropertyTree(property: Tags):TagsTree{
+  let tree:TagsTree = {0: {children: {}, localId: '0'}}
+  let refs:any = {}
+  Object.values(property).forEach((tag:Tag) => {
+      tag.parents.forEach(parent => {
+          if(parent === 0){
+              tree[0].children[tag.id] = {...tag, children: {}, localId: `${0}.${tag.id}`}
+              refs[tag.id] = tree[tag.id]
+          }
+          else{
+              refs[parent].children[tag.id] = {...tag, children: {}, localId: `${refs[parent].localId}.${tag.id}`}
+              refs[tag.id] = refs[parent].children[tag.id]
+          }
+      })
+  })
+  return tree
+}
---
+++ src/utils/api.ts
@@ -0,0 +1,47 @@
+/**
+ * Fichier servant à regrouper les fonctions permettant de communiquer avec le serveur
+ */
+
+// à virer
+import { Property, PropertyType, Tag } from './models'
+
+import axios from 'axios'
+
+export const SERVER_PREFIX = "http://localhost:8000"
+axios.defaults.baseURL = SERVER_PREFIX
+
+
+export const apiGetImages = async () => {
+    const res = await axios.get(`/images`)
+    return res.data
+}
+
+export const apiGetTags = async () => {
+    const res = await axios.get('/tags')
+    return res.data
+}
+
+export const apiGetProperties = async () => {
+    const res = await axios.get('/property')
+    return res.data
+}
+
+export const apiAddTag = async (
+    propertyId: number, 
+    tagValue: string, 
+    color: string, 
+    parentId: number
+    ):Promise<Tag> => {
+    const res = await axios.post('/tags', {
+        propertyId,
+        value: tagValue,
+        ...(color && {color}),
+        ...(parentId && {parentId})
+    })
+    return res.data
+}
+
+export const apiAddProperty = async(name: string, type: PropertyType):Promise<Property> => {
+    const res = await axios.post('/property', {name, type})
+    return res.data
+}
