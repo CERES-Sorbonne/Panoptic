9e44b1279e520a822a3e23f4b6cf68f0347fd7f1
Félix <someonefefe@gmail.com>
2023-04-04 16:43:35 +0200

    Working config for new versions of node

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     server: {
         proxy: {
         '/images': {
-            target: 'http://localhost:8000/',
+            target: 'http://127.0.0.1:8000/',
             changeOrigin: true,
             secure: false,
             rewrite: (path) => {
