7f23b49cd709ab857d3f59476b283ae64a935e9b
Tyrannas <someonefefe@gmail.com>
2023-04-17 15:49:29 +0200

    loading icon when adding folders

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -6,6 +6,16 @@ import * as models from '../../data/models'
 import TagProperty from '../properties/TagProperty.vue';
 import Property from '../properties/Property.vue';
 import { Modals } from '../../data/models';
+import { reactive, ref } from 'vue';
+
+
+let isFolderLoading = ref(false)
+
+const addFolder = async () => {
+    isFolderLoading.value = true
+    await globalStore.importFolders()
+    isFolderLoading.value = false
+}
 
 </script>
 
@@ -19,8 +29,11 @@ import { Modals } from '../../data/models';
                 <li class="list-group-item">
                     <ExpandOption title-size="h6">
                         <template #name> Folders </template>
-                        <template #icons><span @click.stop="globalStore.importFolders"
-                                class=" h4 bi bi-folder-plus me-3"></span></template>
+                        <template #icons>
+                            <span @click.stop="addFolder"
+                                class=" h4 bi bi-folder-plus me-3" v-if="!isFolderLoading"/>
+                            <span v-else class="spinner-border me-3 text-secondary" role="status" style="width:1.5rem;height:1.5rem"/>
+                        </template>
                         <template #content>
                             <ul class="list-group option-content" @click.stop>
                                 <li class="list-group-item" v-for="folder in globalStore.params.folders">{{ folder }}</li>
