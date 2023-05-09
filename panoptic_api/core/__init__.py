afd946001b4777c8be82f26366a79ecbfabe8501
david <godicked@gmail.com>
2023-05-09 23:01:20 +0200

    seems like a working image import with compute. Seems to really be non blocking
    Async rewrite of the backend api

--- panoptic_api/core/__init__.py
+++ panoptic_api/core/__init__.py
@@ -1,46 +1,53 @@
-import concurrent.futures
+import atexit
+import atexit
 import hashlib
 import json
 import os
+from concurrent.futures import ProcessPoolExecutor
 from typing import List
 
 from PIL import Image as pImage
 from fastapi import HTTPException
 
-from panoptic_api.core.queue import TransformManager
-from panoptic_api.models import PropertyType, JSON, Image, Tag, Images, PropertyValue, Property, Tags, Properties, UpdateTagPayload, UpdatePropertyPayload
 import panoptic_api.core.db
+from panoptic_api.core.queue import TransformManager
+from panoptic_api.models import PropertyType, JSON, Image, Tag, Images, PropertyValue, Property, Tags, Properties, \
+    UpdateTagPayload, UpdatePropertyPayload
+from .image_importer import ImageImporter
+from .. import panoptic
 
-transform_manager = TransformManager(root=os.getcwd())
+executor = ProcessPoolExecutor(max_workers=4)
+atexit.register(executor.shutdown)
+importer = ImageImporter(executor)
 
 
-def create_property(name: str, property_type: PropertyType) -> Property:
-    return db.add_property(name, property_type.value)
+async def create_property(name: str, property_type: PropertyType) -> Property:
+    return await db.add_property(name, property_type.value)
 
 
-def update_property(payload: UpdatePropertyPayload) -> Property:
-    existing_property = db.get_property_by_id(payload.id)
+async def update_property(payload: UpdatePropertyPayload) -> Property:
+    existing_property = await db.get_property_by_id(payload.id)
     if not existing_property:
         raise HTTPException(status_code=400, detail="Trying to modify non existent property")
     new_property = existing_property.copy(update=payload.dict(exclude_unset=True))
-    db.update_property(new_property)
+    await db.update_property(new_property)
     return new_property
 
 
-def get_properties() -> Properties:
-    properties = db.get_properties()
+async def get_properties() -> Properties:
+    properties = await db.get_properties()
     return {prop.id: prop for prop in properties}
 
 
-def delete_property(property_id: str):
-    db.delete_property(property_id)
+async def delete_property(property_id: str):
+    await db.delete_property(property_id)
 
 
-def get_images() -> Images:
+async def get_images() -> Images:
     """
     Get all images from database
     """
-    rows = db.get_images()
+    rows = await db.get_images()
     result = {}
     for row in rows:
         sha1, paths, height, width, url, extension, name, property_id, value = row
@@ -53,21 +60,21 @@ def get_images() -> Images:
     return result
 
 
-def add_property_to_image(property_id: int, sha1: str, value: JSON) -> str:
+async def add_property_to_image(property_id: int, sha1: str, value: JSON) -> str:
     # first check that the property and the image exist:
-    if db.get_property_by_id(property_id) and db.get_image_by_sha1(sha1):
+    if await db.get_property_by_id(property_id) and await db.get_image_by_sha1(sha1):
         # check if a value already exists
-        if db.get_image_property(sha1, property_id):
-            db.update_image_property(sha1, property_id, value)
+        if await db.get_image_property(sha1, property_id):
+            await db.update_image_property(sha1, property_id, value)
         else:
-            db.add_image_property(sha1, property_id, value)
+            await db.add_image_property(sha1, property_id, value)
         return value
     else:
         raise HTTPException(status_code=400, detail="Trying to set a value on a non existent property or sha1")
 
 
-def delete_image_property(property_id: int, sha1: str):
-    db.delete_image_property(property_id, sha1)
+async def delete_image_property(property_id: int, sha1: str):
+    await db.delete_image_property(property_id, sha1)
 
 
 def _preprocess_image(file_path):
@@ -76,102 +83,122 @@ def _preprocess_image(file_path):
     extension = name.split('.')[-1]
     width, height = image.size
     sha1_hash = hashlib.sha1(image.tobytes()).hexdigest()
-    transform_manager.process_image(sha1_hash, image)
     # TODO: gérer l'url statique quand on sera en mode serveur
     # url = os.path.join('/static/' + file_path.split(os.getenv('PANOPTIC_ROOT'))[1].replace('\\', '/'))
     url = f"/images/{file_path}"
+
+    ahash, vector = _compute_ml(image)
+
     return file_path, name, extension, width, height, sha1_hash, url
 
 
-def add_image(file_path) -> Image:
+def _compute_ml(image):
+    # TODO: maybe move this function in core ? later if it works
+    # TODO: use multiprocessing here ?
+    ahash = panoptic.to_average_hash(image)
+    # TODO: find a way to access all the images for PCA
+    vector = panoptic.to_vector(image)
+    # TODO: maybe add another queue to avoid bottleneck on db
+    return ahash, vector
+
+
+async def add_image(file_path) -> Image:
     _, name, extension, width, height, sha1_hash, url = _preprocess_image(file_path)
     # Vérification si sha1_hash existe déjà dans la table images
-    return add_image_to_db(file_path, name, extension, width, height, sha1_hash, url)
+    return await add_image_to_db(file_path, name, extension, width, height, sha1_hash, url)
 
 
-def add_image_to_db(file_path, name, extension, width, height, sha1_hash, url) -> Image:
-    image = db.get_image_by_sha1(sha1_hash)
+async def add_image_to_db(file_path, name, extension, width, height, sha1_hash, url) -> Image:
+    image = await db.get_image_by_sha1(sha1_hash)
     # Si sha1_hash existe déjà, on ajoute file_path à la liste de paths
     if image:
         if file_path not in image.paths:
             image.paths.append(file_path)
             # Mise à jour de la liste de paths
-            db.update_image_paths(sha1_hash, json.dumps(image.paths))
+            await db.update_image_paths(sha1_hash, json.dumps(image.paths))
 
     # Si sha1_hash n'existe pas, on l'ajoute avec la liste de paths contenant file_path
     else:
-        db.add_image(sha1_hash, height, width, name, extension, json.dumps([file_path]), url)
-    return db.get_image_by_sha1(sha1_hash)
+        await db.add_image(sha1_hash, height, width, name, extension, json.dumps([file_path]), url)
+    return await db.get_image_by_sha1(sha1_hash)
 
 
-def add_folder(folder):
-    folders = db.get_parameters().folders
-    db.update_folders(list({folder, *folders}))
-    all_files = [os.path.join(path, name) for path, subdirs, files in os.walk(folder) for name in files]
-    all_images = [i for i in all_files if
-                  i.lower().endswith('.png') or i.lower().endswith('.jpg') or i.lower().endswith('.jpeg')]
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        transformed = [executor.submit(_preprocess_image, i) for i in all_images]
-    for future in concurrent.futures.as_completed(transformed):
-        add_image_to_db(*future.result())
-    return len(all_images)
+async def add_folder(folder):
+    folders = (await db.get_parameters()).folders
+    await db.update_folders(list({folder, *folders}))
+
+    found = importer.import_folder(save_callback, folder)
+    print(f'found {found} images')
+    return found
 
 
-def create_tag(property_id, value, parent_id) -> Tag:
-    existing_tag = db.get_tag(property_id, value)
+async def save_callback(image, file_path, name, extension, width, height, sha1_hash, url):
+    await add_image_to_db(file_path, name, extension, width, height, sha1_hash, url)
+    print('saved to db image: ', name)
+    importer.compute_image(get_compute_callback(sha1_hash), image_path=file_path)
+
+
+def get_compute_callback(sha1):
+    async def callback(ahash, vector):
+        await db.update_image_hashs(sha1, str(ahash), vector)
+    return callback
+
+
+async def create_tag(property_id, value, parent_id) -> Tag:
+    existing_tag = await db.get_tag(property_id, value)
     if existing_tag is not None:
-        if db.tag_in_ancestors(existing_tag.id, parent_id):
+        if await db.tag_in_ancestors(existing_tag.id, parent_id):
             raise HTTPException(status_code=400, detail="Adding a tag that is an ancestor of himself")
         existing_tag.parents = list({*existing_tag.parents, parent_id})
-        db.update_tag(existing_tag)
+        await db.update_tag(existing_tag)
         tag_id = existing_tag.id
     else:
         parents = [parent_id]
-        tag_id = db.add_tag(property_id, value, json.dumps(parents))
-    return db.get_tag_by_id(tag_id)
+        tag_id = await db.add_tag(property_id, value, json.dumps(parents))
+    return await db.get_tag_by_id(tag_id)
 
 
-def update_tag(payload: UpdateTagPayload) -> Tag:
-    existing_tag = db.get_tag_by_id(payload.id)
+async def update_tag(payload: UpdateTagPayload) -> Tag:
+    existing_tag = await db.get_tag_by_id(payload.id)
     if not existing_tag:
         raise HTTPException(status_code=400, detail="Trying to modify non existent tag")
-    if db.tag_in_ancestors(existing_tag.id, payload.parent_id):
+    if await db.tag_in_ancestors(existing_tag.id, payload.parent_id):
         raise HTTPException(status_code=400, detail="Adding a tag that is an ancestor of himself")
     # change only fields of the tags that are set in the payload
     new_tag = existing_tag.copy(update=payload.dict(exclude_unset=True))
-    db.update_tag(new_tag)
+    await db.update_tag(new_tag)
     return new_tag
 
 
-def delete_tag(tag_id: int) -> List[int]:
+async def delete_tag(tag_id: int) -> List[int]:
     # first delete the tag
-    modified_tags = [db.delete_tag_by_id(tag_id)]
+    modified_tags = [await db.delete_tag_by_id(tag_id)]
     # when deleting a tag, get all children of this tag
-    children = db.get_tags_by_parent_id(tag_id)
+    children = await db.get_tags_by_parent_id(tag_id)
     # and remove parent ref of tag in children
-    [modified_tags.extend(delete_tag_parent(c, tag_id)) for c in children]
+    [modified_tags.extend(await delete_tag_parent(c, tag_id)) for c in children]
     # then get all images properties tagged with it
-    image_properties = db.get_image_properties_with_tag(tag_id)
+    image_properties = await db.get_image_properties_with_tag(tag_id)
     # and delete the tag ref from them
     for image_property in image_properties:
         image_property.value.remove(tag_id)
-        db.update_image_property(image_property.sha1, image_property.property_id, image_property.value)
+        await db.update_image_property(image_property.sha1, image_property.property_id, image_property.value)
     return modified_tags
 
 
-def delete_tag_parent(tag_id: int, parent_id: int) -> List[int]:
-    tag = db.get_tag_by_id(tag_id)
+async def delete_tag_parent(tag_id: int, parent_id: int) -> List[int]:
+    tag = await db.get_tag_by_id(tag_id)
     tag.parents.remove(parent_id)
     if not tag.parents:
-        return delete_tag(tag.id)
+        return await delete_tag(tag.id)
     else:
-        db.update_tag(tag)
+        await db.update_tag(tag)
         return []
 
 
-def get_tags(prop: str = None) -> Tags:
+async def get_tags(prop: str = None) -> Tags:
     res = {}
-    tag_list = db.get_tags(prop)
+    tag_list = await db.get_tags(prop)
     for tag in tag_list:
         if tag.property_id not in res:
             res[tag.property_id] = {}
--- panoptic_api/core/db_utils.py
+++ panoptic_api/core/db_utils.py
@@ -1,43 +1,48 @@
+import asyncio
 import json
 import os
-import sqlite3
-from json import JSONDecodeError
-
 import numpy as np
+import aiosqlite
+from json import JSONDecodeError
 
 ALL_TABLES = ['images', 'images_properties', 'properties', 'parameters', 'tags']
 
-sqlite3.register_adapter(np.array, lambda arr: arr.tobytes())
-sqlite3.register_converter("array", np.frombuffer)
+aiosqlite.register_adapter(np.array, lambda arr: arr.tobytes())
+aiosqlite.register_converter("array", np.frombuffer)
 
-conn = sqlite3.connect("panoptic.db", detect_types=sqlite3.PARSE_DECLTYPES)
+conn: aiosqlite.Connection | None = None
 
 
-def create_tables_if_db_empty():
+async def init():
+    global conn
+    conn = await aiosqlite.connect("panoptic.db")
+    await create_tables_if_db_empty()
+
+
+async def create_tables_if_db_empty():
     query = "SELECT name FROM sqlite_master WHERE type='table'"
-    cursor = execute_query(query)
-    all_tables = cursor.fetchall()
-    if len(all_tables) < len(ALL_TABLES):
+    cursor = await execute_query(query)
+    all_tables = await cursor.fetchall()
+    if len(list(all_tables)) < len(ALL_TABLES):
         with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'scripts', 'create_db.sql'), 'r') as f:
             sql_script = f.read()
-            cursor = conn.cursor()
-            cursor.executescript(sql_script)
-            conn.commit()
+            async with conn.execute(sql_script) as cursor:
+                await conn.commit()
 
 
 # Fonction utilitaire pour exécuter une requête SQL et commettre les modifications
-def execute_query(query: str, parameters: tuple = None):
-    cursor = conn.cursor()
+async def execute_query(query: str, parameters: tuple = None):
+    cursor = await conn.cursor()
     if parameters:
-        cursor.execute(query, parameters)
+        await cursor.execute(query, parameters)
     else:
-        cursor.execute(query)
-    conn.commit()
+        await cursor.execute(query)
+    await conn.commit()
     return cursor
 
 
 def decode_if_json(value):
     try:
         return json.loads(value)
-    except (TypeError, JSONDecodeError):
+    except (TypeError, JSONDecodeError, UnicodeDecodeError):
         return value
