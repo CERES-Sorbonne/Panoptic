5b0f7e5ec3395615529288080989bd69c5b316bd
david <godicked@gmail.com>
2023-05-16 20:56:45 +0200

    simplified ui for filters. less options

--- panoptic_front/src/components/forms/FilterForm.vue
+++ panoptic_front/src/components/forms/FilterForm.vue
@@ -5,6 +5,7 @@ import GlobalFilterInputDropdown from '../inputs/GlobalFilterInputDropdown.vue';
 import { globalStore } from '@/data/store';
 import { defaultOperator } from '@/utils/filter';
 import PropertySelection from '../inputs/PropertySelection.vue';
+import { computed } from 'vue';
 
 const props = defineProps({
     filter: Object as () => FilterGroup
@@ -35,21 +36,26 @@ function addNewGroupFilter() {
 
 <template>
     <div class="d-flex flex-row">
-        <div class="">
+        <div class="label me-0 pe-1">Filtres</div>
+        <!-- <div class="">
             <GlobalFilterInputDropdown :model-value="props.filter" @update:model-value="e => Object.assign(props.filter, e)"/>
-        </div>
-        <div class="btn btn-sm border rounded bg-white hover-light me-1" @click="props.filter.groupOperator = props.filter.groupOperator == FilterOperator.and ? FilterOperator.or : FilterOperator.and">{{
-            props.filter.groupOperator == FilterOperator.and ? 'All' : 'Any' }}</div>
-        <div v-for="filter, index in props.filter.filters">
+        </div> -->
+        <!-- <div class="btn btn-sm border rounded bg-white hover-light me-1" @click="props.filter.groupOperator = props.filter.groupOperator == FilterOperator.and ? FilterOperator.or : FilterOperator.and">{{
+            props.filter.groupOperator == FilterOperator.and ? 'All' : 'Any' }}</div> -->
+        <!-- <div v-for="filter, index in props.filter.filters">
             <FilterInputDropdown v-model="props.filter.filters[index]" @delete="props.filter.filters.splice(index, 1)" />
-        </div>
-        <div class="btn btn-sm no-border me-1 p-1 hover-light text-secondary" data-bs-toggle="dropdown" data-bs-auto-close="true">
-            <i class="bi bi-plus"></i> Add filter
+        </div> -->
+        <!-- <i class="bi bi-plus"></i> -->
+        <GlobalFilterInputDropdown :model-value="props.filter" @update:model-value="e => Object.assign(props.filter, e)"/>
+      
+        <div class="btn btn-sm no-border me-1 p-1 hover-light text-secondary" data-bs-toggle="dropdown"
+            data-bs-auto-close="true">
+            <i class="bi bi-plus"></i>
         </div>
         <div class="dropdown-menu p-0">
-            <PropertySelection @select="addNewFilter"/>
-            <hr class="dropdown-divider">
-            <div class="hover-light p-1 ps-2 mb-1 btn-icon" @click="addNewGroupFilter">Filter group</div>
+            <PropertySelection @select="addNewFilter" />
+            <!-- <hr class="dropdown-divider">
+            <div class="hover-light p-1 ps-2 mb-1 btn-icon" @click="addNewGroupFilter">Filter group</div> -->
         </div>
     </div>
 </template>
--- panoptic_front/src/components/inputs/GlobalFilterInputDropdown.vue
+++ panoptic_front/src/components/inputs/GlobalFilterInputDropdown.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { FilterGroup, FilterOperator } from '@/data/models';
-import { reactive, watch } from 'vue';
+import { Filter, FilterGroup, FilterOperator } from '@/data/models';
+import { reactive, watch, computed } from 'vue';
 import FilterGroupInput from './FilterGroupInput.vue';
 import { globalStore } from '@/data/store';
 
@@ -10,17 +10,45 @@ const props = defineProps({
 
 const emits = defineEmits(['update:modelValue'])
 
+
+const selectedFilterSet = computed(() => {
+    let list = recursiveListFilters(props.modelValue)
+    let unique = {} as any
+    list.forEach(filter => unique[filter.propertyId] = filter)
+    return Object.values(unique) as Array<Filter>
+})
+
+function recursiveListFilters(root: FilterGroup): Array<Filter> {
+    let res = []
+    for (let filter of root.filters) {
+        if (filter.isGroup) {
+            res.push(...recursiveListFilters(filter))
+        }
+        else {
+            res.push(filter as Filter)
+        }
+    }
+    return res
+}
+
 watch(() => props.modelValue, () => {
     emits('update:modelValue', props.modelValue)
-},{deep: true})
+}, { deep: true })
 
 </script>
 
 <template>
     <div class="btn-group">
-        <button class="btn btn-sm border rounded bg-white hover-light me-1 dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside"
-            aria-expanded="false">
-            <span class="badge bg-medium text-dark me-1">{{ props.modelValue.filters.length }}</span>Filters
+        <button class="btn-sm no-border rounded bg-white p-0 m-0" type="button" data-bs-toggle="dropdown"
+            data-bs-auto-close="outside" aria-expanded="false">
+            <div class="bg-medium d-flex flex-row rounded m-0 p-0" style="cursor:pointer;">
+                <template v-for="filter, index in selectedFilterSet">
+                    <span v-if="index > 0" class="label">|</span>
+                    <div class="label">
+                        {{ globalStore.properties[filter.propertyId].name }}
+                    </div>
+                </template>
+            </div>
         </button>
         <div class="dropdown-menu m-0 p-0" aria-labelledby="defaultDropdown">
             <div class="m-1 p-0" v-if="Object.keys(globalStore.properties).length > 0">
