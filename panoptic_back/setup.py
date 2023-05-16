c64d849d1e7e5b7d0115cb24f392f9b11fcc16ca
Félix <someonefefe@gmail.com>
2023-05-16 16:10:02 +0200

    Working setup

--- panoptic_back/setup.py
+++ panoptic_back/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-
+import os
 
 def parse_requirements(req_file):
     with open(req_file) as fp:
@@ -20,10 +20,26 @@ def setup_package():
                     licence='MIT',
                     install_requires=INSTALL_REQUIRES,
                     setup_requires=SETUP_REQUIRES,
+                    entry_points={
+                        'console_scripts':[
+                            'panoptic = panoptic.main:start'
+                        ]
+                    },
+                    include_package_data=True,
+                    # package_dir="panoptic/html",
+                    package_data={
+                        "": ['html/*', 'html/assets/*'],
+                    },
                     packages=find_packages())
 
     setup(**metadata)
 
+
+def _copy_html_data(directory):
+    base_dir = os.path.join('', directory)
+    for (dirpath, dirnames, files) in os.walk(base_dir):
+        for f in files:
+            yield os.path.join(dirpath.split('/', 1)[1], f)
 
 if __name__ == "__main__":
     setup_package()
