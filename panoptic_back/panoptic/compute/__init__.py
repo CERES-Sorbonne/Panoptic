f8318c1afbae159e045a1a26ccc22afe6c12fe96
Félix <someonefefe@gmail.com>
2023-05-23 16:41:54 +0200

    Working image similarity getter

--- panoptic_back/panoptic/compute/__init__.py
+++ panoptic_back/panoptic/compute/__init__.py
@@ -1,2 +1,2 @@
 from .transform import transform_image, transform_directory, to_average_hash, to_sha1, to_vector
-from .similarity import make_clusters
+from .similarity import make_clusters, get_similar_images, create_similarity_tree
