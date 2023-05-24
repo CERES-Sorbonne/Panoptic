8be319ccacd1692053d4dddb6915378435c162cc
david <godicked@gmail.com>
2023-05-24 14:18:27 +0200

    charge more image UI reworked, lateral bar to go to parent group is now visible and clickable

--- panoptic_front/src/components/images/ImageGroup.vue
+++ panoptic_front/src/components/images/ImageGroup.vue
@@ -11,10 +11,12 @@ const props = defineProps({
     group: Object as () => Group,
     small: String,
     imageSize: Number,
-    groupId: {type: String, default: '0'}
+    groupId: { type: String, default: '0' }
 })
 
 const clusterNb = ref(10)
+const topElem = ref(null)
+const hoverBorder = ref(false)
 
 const images = computed(() => props.group.images)
 const subgroups = computed(() => props.group.groups)
@@ -24,7 +26,7 @@ const property = computed(() => globalStore.properties[props.group.propertyId])
 
 const groupName = computed(() => {
     let name = props.group.name
-    if(props.group.propertyId == undefined) {
+    if (props.group.propertyId == undefined) {
         return props.group.name
     }
     if (props.group.name == "undefined") {
@@ -47,57 +49,72 @@ function clear() {
 async function computeClusters() {
     let mlGroups = await globalStore.computeMLGroups(props.group.images.map(i => i.sha1), clusterNb.value)
     props.group.groups = []
-    for(let [index, group] of mlGroups.entries()){
+    for (let [index, group] of mlGroups.entries()) {
         let realGroup: Group = {
             name: 'cluster ' + index.toString(),
             images: group,
-            count:group.length,
+            count: group.length,
             groups: undefined
         }
         props.group.groups.push(realGroup)
     }
 }
 
+function scrollToTop() {
+    topElem.value.scrollIntoView()
+}
+
 </script>
 
 <template>
-    <ExpandOption :small="props.small" :left-align="true" :reset-on-hide="true" class="image-group">
+    <ExpandOption :small="props.small" :left-align="true" :reset-on-hide="true">
         <template #name>
-            <div class="d-flex flex-row mb-1">
+            <div class="d-flex flex-row mb-1" ref="topElem">
                 <div v-if="property != undefined"><b>{{ property.name }}</b> : {{ groupName }}</div>
                 <div v-else><b>{{ groupName }}</b></div>
                 <div class="ms-2">({{ props.group.count }})</div>
-                <div v-if="hasImages && hasSubgroups" class="button-offset ms-2"><button @click="clear">Clear</button></div>
-                <div v-if="hasImages && !hasSubgroups" class="ms-2"><StampDropdown :images="images"/></div>
-                <div v-if="hasImages && !hasSubgroups" class="button-offset ms-2"><button @click="computeClusters">Compute Groups</button></div>
-                <div v-if="hasImages && !hasSubgroups" class="button-offset ms-2"><input class="" type="number" v-model="clusterNb" style="width: 30px;"/></div>
-            </div>
-             
-        </template>
-        <template #content>
-            <div v-if="hasSubgroups" class="ms-3">
-                <ImageGroup v-for="group, index in subgroups" :group="group" small=true :image-size="props.imageSize" :groupId="props.groupId + '-' + index"/>
-            </div>
-            <div v-else-if="hasImages">
-                <div class="ms-3">
-                    <PaginatedImages :images="images" :image-size="imageSize" :groupId="props.groupId"/>
+                <div v-if="hasImages && hasSubgroups" class="ms-2"><button @click="clear">Clear</button></div>
+                <div v-if="hasImages && !hasSubgroups" class="ms-2">
+                    <StampDropdown :images="images" />
+                </div>
+                <div v-if="hasImages && !hasSubgroups" class="ms-2">
+                    <button @click="computeClusters">Compute Groups</button>
+                </div>
+                <div v-if="hasImages && !hasSubgroups" class="ms-2">
+                    <input class="" type="number" v-model="clusterNb" style="width: 30px;" />
                 </div>
             </div>
-            <div v-else>
-                Error.. No Subgroups, No images, Why ?
+
+        </template>
+        <template #content>
+            <div class="d-flex flex-row">
+                <div style="cursor: pointer;" class="ps-2" @click="scrollToTop" @mouseenter="hoverBorder = true"
+                    @mouseleave="hoverBorder = false">
+                    <div class="image-group pe-2 h-100" :class="hoverBorder ? 'active' : ''"></div>
+                </div>
+                <div v-if="hasSubgroups" class="ms-3">
+                    <ImageGroup v-for="group, index in subgroups" :group="group" small=true :image-size="props.imageSize"
+                        :groupId="props.groupId + '-' + index" />
+                </div>
+                <div v-else-if="hasImages">
+                    <div class="ms-3">
+                        <PaginatedImages :images="images" :image-size="imageSize" :groupId="props.groupId" />
+                    </div>
+                </div>
+                <div v-else>
+                    Error.. No Subgroups, No images, Why ?
+                </div>
             </div>
         </template>
     </ExpandOption>
 </template>
 
 <style scoped="true">
-/* .image-group {
+.image-group {
     border-left: 1px solid var(--border-color);
-} */
-
-.button-offset {
-    position: relative;
-    top: -2px
 }
 
+.active {
+    border-left: 1px solid blue;
+}
 </style>
