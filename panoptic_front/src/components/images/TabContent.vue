c80048fe07b25320a60f9050416916d21145225e
david <godicked@gmail.com>
2023-05-25 02:58:39 +0200

    group diffing to keep relevant clusters

--- panoptic_front/src/components/images/TabContent.vue
+++ panoptic_front/src/components/images/TabContent.vue
@@ -17,25 +17,30 @@ const filters = computed(() => props.tab.data.filter)
 const groups = computed(() => props.tab.data.groups)
 const sorts = computed(() => props.tab.data.sortList)
 
-const imageGroups = reactive([])
+const imageGroups = reactive({}) as Group
+const groupList = computed(() => {
+    if(!imageGroups.id) 
+        return []
+    return [imageGroups]
+})
 
 const filteredImages = computed(() => {
     let images = Object.values(globalStore.images)
 
-    if(Object.keys(props.tab.data.selectedFolders).length > 0) {
+    if (Object.keys(props.tab.data.selectedFolders).length > 0) {
         let folderIds = Object.keys(props.tab.data.selectedFolders).map(Number)
         let allIds = [...folderIds] as Array<number>
         folderIds.forEach(id => allIds.push(...globalStore.getFolderChildren(id)))
-        if(allIds.length > 0) {
-            images = images.filter(img => img.paths.some((p:any) => allIds.includes(Number(p))))
+        if (allIds.length > 0) {
+            images = images.filter(img => img.paths.some((p: any) => allIds.includes(Number(p))))
         }
     }
 
     let filtered = images.filter(img => computeGroupFilter(img, filters.value))
 
-    if(sorts.value.length > 0) {
+    if (sorts.value.length > 0) {
         filtered = sortImages(filtered, globalStore.properties[sorts.value[0].property_id])
-        if(!sorts.value[0].ascending) {
+        if (!sorts.value[0].ascending) {
             filtered.reverse()
         }
     }
@@ -43,21 +48,81 @@ const filteredImages = computed(() => {
     return filtered
 })
 function computeGroups() {
-    imageGroups.length = 0
+    let rootGroup = generateGroups()
+    Object.assign(imageGroups, replaceIfChanged(imageGroups, rootGroup))
+    
+    // if (Array.isArray(rootGroup.groups)) {
+    //     imageGroups.push(...rootGroup.groups)
+    // }
+    // else {
+    //     imageGroups.push(rootGroup)
+    // }
+}
+function isNode(group: Group) {
+    return group.images && group.images.length == 0 && Array.isArray(group.groups) && group.groups.length > 0
+}
+function isLeaf(group: Group) {
+    return group.images && group.images.length > 0
+}
+
+function replaceIfChanged(oldGroup: Group, newGroup: Group) {
+    if(!oldGroup) {
+        return newGroup
+    }
+
+    let changed = false
+    if (isNode(oldGroup) && isNode(newGroup)) {
+        if (oldGroup.propertyId != newGroup.propertyId || oldGroup.name != newGroup.name || oldGroup.count != newGroup.count) {
+            changed = true
+        }
+    }
+    else if (isLeaf(oldGroup) && isLeaf(newGroup)) {
+        if (oldGroup.propertyId == newGroup.propertyId || oldGroup.count == newGroup.count || oldGroup.name == oldGroup.name) {
+            let oldImgs = {} as any
+            oldGroup.images.forEach(img => oldImgs[img.sha1] = true)
+            for (let img of newGroup.images) {
+                if (!oldImgs[img.sha1]) {
+                    changed = true
+                    break
+                }
+            }
+        }
+        else {
+            changed = true
+        }
+    }
+    // type of nodes dont match
+    else {
+        changed = true
+    }
+    if(changed) {
+        return newGroup
+    }
+    if(isLeaf(oldGroup)) {
+        return oldGroup
+    }
+
+    let oldSubGroups = {} as {[k:string]: Group}
+    oldGroup.groups.forEach(g => oldSubGroups[g.id] = g)
+
+    oldGroup.groups = newGroup.groups.map(g => replaceIfChanged(oldSubGroups[g.id], g))
+
+    return oldGroup
+}
+
+function generateGroups() {
     let rootGroup = {
         name: 'All',
         images: filteredImages.value,
         groups: undefined,
         count: filteredImages.value.length,
-        propertyId: undefined
+        propertyId: undefined,
+        id: '__all__'
     } as Group
     if (groups.value.length > 0) {
         rootGroup = computeSubgroups(rootGroup, groups.value)
-        imageGroups.push(...rootGroup.groups)
     }
-    else {
-        imageGroups.push(rootGroup)
-    }
+    return rootGroup
 }
 
 
@@ -90,8 +155,9 @@ function computeSubgroups(parentGroup: Group, groupList: number[]) {
             images: groups[group],
             groups: undefined,
             count: groups[group].length,
-            propertyId: propertyId
-        })
+            propertyId: propertyId,
+            id: propertyId + '-' + group
+        } as Group)
     }
 
     if (groupList.length > 1) {
@@ -117,16 +183,17 @@ watch(groups, computeGroups, { deep: true })
 
 <template>
     <div class="m-2">
-        <ContentFilter :tab="props.tab" @compute-ml=""/>
+        <ContentFilter :tab="props.tab" @compute-ml="" />
     </div>
-    <hr class="custom-hr"/>
+    <hr class="custom-hr" />
     <div class="ms-2 mt-2">
-        <div v-if="imageGroups.length && imageGroups[0].name == '__all__'">
-            <PaginatedImages :images="imageGroups[0].images" :imageSize="props.tab.data.imageSize" :groupId="'0'"/>
+        <div v-if="groupList.length && groupList[0].name == '__all__'">
+            <PaginatedImages :images="groupList[0].images" :imageSize="props.tab.data.imageSize" :groupId="'0'" />
         </div>
         <div v-else>
-            <div v-for="(group, index) in imageGroups">
-                <ImageGroup :leftAlign="true" :group="group" :imageSize="props.tab.data.imageSize" :group-id="String(index)" />
+            <div v-for="(group, index) in groupList">
+                <ImageGroup :leftAlign="true" :group="group" :imageSize="props.tab.data.imageSize"
+                    :group-id="String(index)" />
             </div>
         </div>
     </div>
