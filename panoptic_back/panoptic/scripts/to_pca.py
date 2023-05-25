d08f001bb9220540258c78ee02a598b0047f9372
Félix <someonefefe@gmail.com>
2023-05-25 11:04:34 +0200

    addnig pca button

--- panoptic_back/panoptic/scripts/to_pca.py
+++ panoptic_back/panoptic/scripts/to_pca.py
@@ -7,8 +7,8 @@ from panoptic.compute import create_pca, to_pca, create_similarity_tree, can_use
 from panoptic.models import ImageVector
 
 
-async def start(force=False):
-    await db_utils.init()
+async def compute_all_pca(force=False):
+    # await db_utils.init()
     all_images: list[ImageVector] = await db.get_images_with_vectors()
     if not force and not can_use_pca(len(all_images), all_images[0].vector):
         print("can't run PCA yet")
@@ -28,4 +28,4 @@ async def start(force=False):
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(start())
+    loop.run_until_complete(compute_all_pca())
