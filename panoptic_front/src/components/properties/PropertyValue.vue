3e7e63fb22ab47a0d7b7a81ec87bd33208bda2c8
david <godicked@gmail.com>
2023-06-13 15:27:09 +0200

    fix reco

--- panoptic_front/src/components/properties/PropertyValue.vue
+++ panoptic_front/src/components/properties/PropertyValue.vue
@@ -3,6 +3,7 @@ import { PropertyType, PropertyValue, Tag } from '@/data/models';
 import { globalStore } from '@/data/store';
 import { computed } from 'vue';
 import TagBadge from '../TagTree/TagBadge.vue';
+import { UNDEFINED_KEY } from '@/utils/groups';
 
 
 
@@ -15,18 +16,23 @@ const type = computed(() => property.value.type)
 
 const isTag = computed(() => type.value == PropertyType.tag || type.value == PropertyType.multi_tags)
 
-function mapTag(id: number): Tag[] {
-    if (id == undefined) {
-        return [{value: 'undefined', color: '#000000'} as Tag]
+function mapTag(id: number | string): Tag[] {
+    console.log(id)
+    if (id == UNDEFINED_KEY) {
+        console.log('go here')
+        return [{value: 'indéfini', color: '#ced4da'} as Tag]
     }
-    return [globalStore.tags[props.value.propertyId][id]]
+    return [globalStore.tags[props.value.propertyId][id as number]]
 }
 </script>
 
 <template>
     <div>
         <span>{{ property.name }}: </span>
-        <span v-if="!isTag">{{ props.value.value }}</span>
+        <span v-if="!isTag">
+            <span v-if="props.value.value != UNDEFINED_KEY">{{ props.value.value }}</span>
+            <span v-else class="text-secondary">indéfini</span>
+        </span>
         <span v-else>
             <span v-for="tag in mapTag(props.value.value)">
                 <TagBadge :tag="tag.value" :color="tag.color" />
