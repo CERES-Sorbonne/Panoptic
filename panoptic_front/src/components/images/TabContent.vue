c14ee06594085e209ba898ab49b990fc2afe5a08
david <godicked@gmail.com>
2023-06-13 23:16:36 +0200

    put group functions in utils

--- panoptic_front/src/components/images/TabContent.vue
+++ panoptic_front/src/components/images/TabContent.vue
@@ -2,15 +2,13 @@
 import { reactive, computed, watch, onMounted, ref, nextTick } from 'vue';
 import { globalStore } from '../../data/store';
 import { computeGroupFilter } from '@/utils/filter';
-import { Group, GroupIndex, PropertyType, Tab, GroupData, Image, PropertyValue } from '@/data/models';
-import { DefaultDict } from '@/utils/helpers'
+import { Group, Tab, GroupData, PropertyValue } from '@/data/models';
 import ContentFilter from './ContentFilter.vue';
 import { sortGroupTree, sortImages } from '@/utils/sort';
 import ImageList from './ImageList.vue';
 
-import moment from 'moment';
 import RecommendedMenu from './RecommendedMenu.vue';
-import { UNDEFINED_KEY } from '@/utils/groups';
+import { generateGroups, mergeGroup } from '@/utils/groups';
 
 const props = defineProps({
     tab: Object as () => Tab,
@@ -84,12 +82,12 @@ function computeGroups(force = false) {
 
     requestIdleCallback(() => {
         console.time('compute groups')
-        let index = {} as GroupIndex
-        let rootGroup = generateGroups(index)
+        let index = generateGroups(filteredImages.value, groups.value)
+        let rootGroup = index['0']
 
         if (!force) {
             for (let id in index) {
-                index[id] = mergeGroup(index[id])
+                index[id] = mergeGroup(index[id], groupData.index)
             }
 
             for (let id in groupData.index) {
@@ -112,109 +110,6 @@ function computeGroups(force = false) {
 
         computeStatus.groups = false
     })
-}
-
-function mergeGroup(update: Group) {
-    let index = groupData.index
-    // console.log('merge: ' + update.id)
-
-    let id = update.id
-
-    if (index[id] == undefined) {
-        return update
-    }
-    // console.log('closed: ' + index[id].closed)
-
-    update.allSimilarSha1s = index[id].allSimilarSha1s
-    update.similarSha1sBlacklist = index[id].similarSha1sBlacklist
-    update.getSimilarImages = index[id].getSimilarImages
-
-    update.closed = index[id].closed
-    let childrenIds = index[id].children
-    if (!childrenIds || childrenIds.length == 0) {
-        return update
-    }
-
-    // let children = childrenIds.map(id => index[id]).filter(c => c != undefined).filter(c => c.propertyId == undefined)
-    let children = index[id].groups
-    if (Array.isArray(index[id].images) && index[id].images.length > 0 && children && children.length > 0) {
-        update.children = children.map(c => c.id)
-        update.groups = children
-    }
-    return update
-}
-
-function generateGroups(index: GroupIndex) {
-    let rootGroup = {
-        name: 'All',
-        images: filteredImages.value,
-        groups: undefined as Group[],
-        count: filteredImages.value.length,
-        propertyId: undefined,
-        id: '0',
-        depth: 0,
-        parentId: undefined,
-        propertyValues: []
-    } as Group
-    if (groups.value.length > 0) {
-        rootGroup = computeSubgroups(rootGroup, groups.value, index)
-    }
-    index[rootGroup.id] = rootGroup
-    return rootGroup
-}
-
-
-
-function computeSubgroups(parentGroup: Group, groupList: number[], index: GroupIndex) {
-    let images = parentGroup.images
-    let propertyId = groupList[0]
-    let groups = new DefaultDict(Array) as { [k: string | number]: any }
-    let type = globalStore.properties[propertyId].type
-
-    for (let img of images) {
-        let value = propertyId in img.properties ? img.properties[propertyId].value : UNDEFINED_KEY
-        if (value == null || value == '') {
-            value = UNDEFINED_KEY
-        }
-        else if (type == PropertyType.checkbox && value != true) {
-            value = false
-        }
-
-        if (Array.isArray(value)) {
-            value.forEach((v: any) => groups[v].push(img))
-        }
-        else if (type == PropertyType.date) {
-            groups[moment(value).format('YYYY/MM')].push(img)
-        }
-        else {
-            groups[value].push(img)
-        }
-    }
-    let res = [] as Group[]
-    for (let key in groups) {
-        let newGroup = {
-            name: key,
-            images: groups[key],
-            groups: undefined as Group[],
-            count: groups[key].length,
-            propertyId: propertyId,
-            id: parentGroup.id + '-' + propertyId + '-' + key,
-            depth: parentGroup.depth + 1,
-            parentId: parentGroup.id,
-            propertyValues: [...parentGroup.propertyValues, { propertyId, value: key }]
-        }
-        res.push(newGroup)
-    }
-
-    if (groupList.length > 1) {
-        res.map(g => computeSubgroups(g, groupList.slice(1), index))
-    }
-
-    parentGroup.groups = res
-    parentGroup.children = res.map(g => g.id)
-    parentGroup.images = []
-    res.forEach(g => index[g.id] = g)
-    return parentGroup
 }
 
 function setRecoImages(images: string[], propertyValues: PropertyValue[], groupId: string) {
