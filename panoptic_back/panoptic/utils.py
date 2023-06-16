132c50ea06c6c48392665a0164374cebcd65c996
Félix <someonefefe@gmail.com>
2023-06-16 12:46:45 +0200

    appdata for all platforms

---
+++ panoptic_back/panoptic/utils.py
@@ -0,0 +1,24 @@
+import pathlib
+import sys
+
+
+def get_datadir() -> pathlib.Path:
+
+    """
+    Returns a parent directory path
+    where persistent application data can be stored.
+
+    # linux: ~/.local/share
+    # macOS: ~/Library/Application Support
+    # windows: C:/Users/<USER>/AppData/Roaming
+    # gently taken from https://stackoverflow.com/questions/19078969/python-getting-appdata-folder-in-a-cross-platform-way
+    """
+
+    home = pathlib.Path.home()
+
+    if sys.platform == "win32":
+        return home / "AppData/Roaming"
+    elif sys.platform == "linux":
+        return home / ".local/share"
+    elif sys.platform == "darwin":
+        return home / "Library/Application Support"
