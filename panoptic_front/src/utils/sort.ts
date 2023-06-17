678417c625aef7836a903bd1d98a362df953e274
david <godicked@gmail.com>
2023-06-17 02:46:27 +0200

    clean old function

--- panoptic_front/src/utils/sort.ts
+++ panoptic_front/src/utils/sort.ts
@@ -44,25 +44,6 @@ function getSortFunction(type: PropertyType): any {
     }
 }
 
-// function getImageSortFunction(property: Property) {
-//     function sortImages(img1: Image, img2: Image) {
-//         let p1 = img1.properties[property.id]?.value
-//         let p2 = img2.properties[property.id]?.value
-
-//         if (property.type == PropertyType.tag) {
-//             let t1 = Array.isArray(p1) && p1.length > 0 ? p1[0] : undefined
-//             let t2 = Array.isArray(p2) && p2.length > 0 ? p2[0] : undefined
-
-//             p1 = globalStore.tags[property.id][t1]?.value
-//             p2 = globalStore.tags[property.id][t2]?.value
-//         }
-
-//         let sortFnc = getSortFunction(property.type)
-//         return sortFnc(p1, p2)
-//     }
-//     return sortImages
-// }
-
 function getImageSortFunction2(sortList: Sort[]) {
     let sortFunctions = sortList.map(s => globalStore.properties[s.property_id].type).map(getSortFunction)
 
