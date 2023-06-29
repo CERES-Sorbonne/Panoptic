9fd0021a5cbf6ee2cc6c95d3f0841c814b9ea779
Félix <someonefefe@gmail.com>
2023-06-29 10:58:47 +0200

    fix create index

--- panoptic_back/panoptic/scripts/create_faiss_index.py
+++ panoptic_back/panoptic/scripts/create_faiss_index.py
@@ -10,15 +10,17 @@ logger = logging.getLogger('Create Faiss')
 
 
 async def compute_faiss_index():
-    os.environ['PANOPTIC_DATA'] = "D:\\Alie\\Documents\\panoptic_GJ"
-    await db_utils.init()
     logger.info('Start')
     all_images: list[ComputedValue] = await db.get_sha1_computed_values()
     create_similarity_tree_faiss(all_images)
     logger.info('Success')
 
+async def start():
+    os.environ['PANOPTIC_DATA'] = "D:\\Alie\\Documents\\panoptic_GJ"
+    await db_utils.init()
+
 if __name__ == '__main__':
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(compute_faiss_index())
+    loop.run_until_complete(start())
 
