a9d9d123ece2123db241e719f2786f4b79fd7592
Félix <someonefefe@gmail.com>
2023-06-29 10:59:16 +0200

    bump version

--- panoptic_back/setup.py
+++ panoptic_back/setup.py
@@ -8,7 +8,7 @@ def parse_requirements(req_file):
 
 
 NAME = "panoptic"
-VERSION = "0.1.1.dev0"
+VERSION = "0.1.1.dev1"
 # Get dependencies from requirement files
 SETUP_REQUIRES = ['setuptools', 'setuptools-git', 'wheel']
 INSTALL_REQUIRES = parse_requirements('requirements.txt')
