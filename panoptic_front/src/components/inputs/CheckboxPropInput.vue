8cebd0ff9d89974c758f08937cf1fc16bdc639b5
david <godicked@gmail.com>
2023-07-28 23:59:40 +0200

    -sorting now works with grid -add checkboxPropInput to grid

--- panoptic_front/src/components/inputs/CheckboxPropInput.vue
+++ panoptic_front/src/components/inputs/CheckboxPropInput.vue
@@ -8,57 +8,53 @@ import { globalStore } from '@/data/store';
 const props = defineProps({
     property: Object as () => Property,
     image: Object as () => Image,
-    noNl: {
-        type: Boolean,
-        default: false,
-    },
     width: Number,
     minHeight: { type: Number, default: 30 }
 
 })
 const emits = defineEmits({ 'update:height': Number })
 
-const localValue = ref('')
 const elem = ref(null)
 // const inputElem = computed(() => elem.value.inputElem)
 
 const propRef = computed(() => getImageProperty(props.image.id, props.property.id))
-const isFocus = computed(() => elem.value?.isFocus)
+// const isFocus = computed(() => elem.value?.isFocus)
 
 function focus() {
     elem.value.focus()
 }
 
-function updateFromStore() {
-    localValue.value = propRef.value.value ?? ''
+function toggle() {
+    globalStore.setPropertyValue(props.property.id, props.image, !propRef.value.value)
 }
 
-function save() {
-    if(propRef.value.value != localValue.value) {
-        globalStore.setPropertyValue(props.property.id, props.image, localValue.value)
-    }
-}
+// function updateFromStore() {
+//     localValue.value = propRef.value.value ?? ''
+// }
+
+// function save() {
+//     globalStore.setPropertyValue(props.property.id, props.image, localValue.value)
+// }
 
 
-onMounted(updateFromStore)
+// onMounted(updateFromStore)
 // watch(propRef, updateFromStore)
 
 
 defineExpose({
-    localValue,
+    // localValue,
     // inputElem
     focus
 })
 
-watch(localValue, save)
+// watch(localValue, save)
 
 
 </script>
 
 <template>
-    <div class="container">
-        <TextInput  class="container" :contenteditable="true" tag="div" :no-html="true" v-model="localValue" :width="props.width"
-            @update:height="h => emits('update:height', h)" ref="elem" :min-height="props.minHeight" />
+    <div class="container d-flex flex-row">
+        <input type="checkbox" v-model="propRef.value" @click.stop.prevent="toggle"/>
     </div>
 </template>
 
