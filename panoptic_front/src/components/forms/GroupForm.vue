fefa76f88751d71e5773bfdc9bf31e8f59cab317
david <godicked@gmail.com>
2023-11-17 23:26:25 +0100

    property selection for groups and sorts now use the new dropdown system

--- panoptic_front/src/components/forms/GroupForm.vue
+++ panoptic_front/src/components/forms/GroupForm.vue
@@ -3,6 +3,8 @@ import { globalStore } from '@/data/store';
 import { computed, onMounted } from 'vue';
 import PropertySelection from '../inputs/PropertySelection.vue';
 import { Sort } from '@/data/models';
+import Dropdown from '../dropdowns/Dropdown.vue';
+import PropertyDropdown from '../dropdowns/PropertyDropdown.vue';
 
 
 const props = defineProps({
@@ -24,7 +26,7 @@ onMounted(() => {
 
 <template>
     <div class="d-flex flex-row group-form">
-        <div class="pt-1 pb-1">{{$t('main.menu.groupby')}}</div>
+        <div class="pt-1 pb-1">{{ $t('main.menu.groupby') }}</div>
         <div class="bg-medium bg d-flex flex-row m-0 ms-1 p-0" v-if="selectedProperties.length">
             <template v-for="property, index in selectedProperties">
                 <i v-if="index > 0" class="bi bi-chevron-right smaller"></i>
@@ -34,14 +36,7 @@ onMounted(() => {
             </template>
             <i v-if="props.isLoading" class="spinner-grow spinner-grow-sm loading ms-1"></i>
         </div>
-        <div class="dropdown">
-            <div class="text-secondary p-1" data-bs-toggle="dropdown" data-bs-auto-close="true">
-                <span class="base-hover plus-btn"><i class="bi bi-plus"></i></span>
-            </div>
-            <div class="dropdown-menu p-0">
-                <PropertySelection @select="prop => globalStore.addGrouping(prop)" :ignore-ids="props.groupIds" />
-            </div>
-        </div>
+        <PropertyDropdown :group-ids="props.groupIds" @select="prop => globalStore.addGrouping(prop)"/>
 
     </div>
 </template>
@@ -64,8 +59,5 @@ onMounted(() => {
     border-radius: 3px;
 }
 
-.plus-btn {
-    padding: 4px !important;
-    border-radius: 3px !important;
-}
+
 </style>
