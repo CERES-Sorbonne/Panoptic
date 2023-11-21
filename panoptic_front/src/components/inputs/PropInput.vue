f8b5f37ec3d7d018b95e00e82ce711ef171d4a2b
david <godicked@gmail.com>
2023-11-21 23:36:55 +0100

    allow tag modification in image modal

--- panoptic_front/src/components/inputs/PropInput.vue
+++ panoptic_front/src/components/inputs/PropInput.vue
@@ -26,7 +26,8 @@ const emits = defineEmits(["update:height"])
     <TextPropInput v-if="property.type == PropertyType.path" :min-height="minHeight" ref="inputElems"
         @update:height="h => emits('update:height', h)" :image="props.image" :property="property" :width="props.width" />
     <TagPropInputDropdown v-if="isTag(property.type)" :property="property" :image="props.image" :width="props.width"
-        :input-id="[0]" :auto-focus="true" @update:height="h => emits('update:height', h)" ref="inputElems" />
+        :can-create="true" :can-customize="true" :can-delete="true" :can-link="true" :input-id="[0]" :auto-focus="true"
+        @update:height="h => emits('update:height', h)" ref="inputElems" />
     <CheckboxPropInput v-if="property.type == PropertyType.checkbox" :min-height="minHeight" ref="inputElems"
         @update:height="h => emits('update:height', h)" :image="props.image" :property="property" :width="props.width" />
     <ColorPropInput v-if="property.type == PropertyType.color" :min-height="minHeight" ref="inputElems"
