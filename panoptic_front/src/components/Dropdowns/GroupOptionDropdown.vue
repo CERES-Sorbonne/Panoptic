5c5c1dbce3791dcb23ba2dd0eb4a3e013c5ea3bc
david <godicked@gmail.com>
2024-01-09 00:43:12 +0100

    remove optional granularity

--- panoptic_front/src/components/Dropdowns/GroupOptionDropdown.vue
+++ panoptic_front/src/components/Dropdowns/GroupOptionDropdown.vue
@@ -13,19 +13,17 @@ const emits = defineEmits<{
     'change': [option: GroupOption]
 }>()
 
-const isSet = ref(false)
 const value = ref(1)
 const unit = ref(DateUnit.Year)
 
 function onHide() {
-    if(props.option.useSteps == isSet.value && props.option.stepSize == value.value && props.option.stepUnit == unit.value) {
+    if(props.option.stepSize == value.value && props.option.stepUnit == unit.value) {
         return
     }
-    emits('change', { useSteps: isSet.value, stepSize: value.value, stepUnit: unit.value })
+    emits('change', { stepSize: value.value, stepUnit: unit.value })
 }
 
 function updateLocal() {
-    isSet.value = props.option.useSteps
     value.value = props.option.stepSize ?? 1
     unit.value = props.option.stepUnit
 }
@@ -46,9 +44,8 @@ watch(value, () => {
         <template #button><i class="bi bi-three-dots-vertical"></i></template>
         <template #popup="{ hide, focus }">
             <div class="">
-                <div class="set-box"><input type="checkbox" v-model="isSet" /> Set granularity</div>
-                <div class="d-flex ipt" :class="isSet ? '' : 'grey-out'">
-                    <div><input type="number" :disabled="!isSet" v-model="value" /></div>
+                <div class="d-flex ipt">
+                    <div><input type="number" v-model="value" /></div>
                     <div class="ms-2" min="1">
                         <TimeUnitDropdown v-model="unit" @hide="focus" />
                     </div>
@@ -59,15 +56,6 @@ watch(value, () => {
 </template>
 
 <style scoped>
-.grey-out {
-    background-color: rgb(240, 240, 240);
-    color: rgb(90, 90, 90);
-}
-
-.set-box {
-    padding: 5px 5px 2px 5px;
-    border-bottom: 1px solid var(--border-color);
-}
 
 .ipt {
     padding: 5px;
