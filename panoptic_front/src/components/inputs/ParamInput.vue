2a49ae13882b23a10e8afb46802065301178c639
david <godicked@gmail.com>
2024-03-06 21:21:54 +0100

    tooltip action button

--- panoptic_front/src/components/inputs/ParamInput.vue
+++ panoptic_front/src/components/inputs/ParamInput.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { defineProps, defineEmits, ref, watch } from 'vue';
-
+import wTT from '@/components/tooltips/withToolTip.vue'
 const props = defineProps<{
     modelValue: any
     type: string
-    label?: string
+    label?: string,
+    desc?: string
 }>();
 
-defineExpose({focus})
+defineExpose({ focus })
 
 const emits = defineEmits(['update:modelValue']);
 
@@ -16,16 +17,16 @@ const localValue = ref(props.modelValue)
 
 function focus() {
     console.log('focus', elem.value)
-    if(elem.value) {
+    if (elem.value) {
         elem.value.focus()
     }
 }
 
 watch(() => props.modelValue, () => localValue.value = props.modelValue)
 watch(localValue, () => {
-    if(localValue.value != props.modelValue) {
+    if (localValue.value != props.modelValue) {
         let toSend = localValue.value
-        if(localValue.value == '' || localValue.value == false) {
+        if (localValue.value == '' || localValue.value == false) {
             toSend = undefined
         }
         emits('update:modelValue', toSend)
@@ -34,22 +35,24 @@ watch(localValue, () => {
 </script>
 
 <template>
-    <div class="d-flex">
-        <div v-if="props.label" class="me-1">{{ props.label }}</div>
-        <!-- <div class="me-1">[{{ props.type }}]</div> -->
-        <div v-if="props.type == 'str'">
-            <input type="text" v-model="localValue" ref="elem"/>
+    <wTT :message="props.desc">
+        <div class="d-flex">
+            <div v-if="props.label" class="me-1">{{ props.label }}</div>
+            <!-- <div class="me-1">[{{ props.type }}]</div> -->
+            <div v-if="props.type == 'str'">
+                <input type="text" v-model="localValue" ref="elem" />
+            </div>
+            <div v-if="props.type == 'int'">
+                <input type="number" step="1" v-model="localValue" ref="elem" />
+            </div>
+            <div v-if="props.type == 'float'">
+                <input type="number" v-model="localValue" ref="elem" />
+            </div>
+            <div v-if="props.type == 'bool'">
+                <input type="checkbox" v-model="localValue" ref="elem" />
+            </div>
         </div>
-        <div v-if="props.type == 'int'">
-            <input type="number" step="1" v-model="localValue" ref="elem" />
-        </div>
-        <div v-if="props.type == 'float'">
-            <input type="number" v-model="localValue" ref="elem" />
-        </div>
-        <div v-if="props.type == 'bool'">
-            <input type="checkbox" v-model="localValue" ref="elem" />
-        </div>
-    </div>
+    </wTT>
 </template>
 
 <style scoped>
