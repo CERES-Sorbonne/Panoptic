3c4b4f7bc023c49ff7cb4d0d648b13b577384663
david <godicked@gmail.com>
2024-03-08 11:49:08 +0100

    create global file path if not exist

--- panoptic_back/panoptic/scripts/test.py
+++ panoptic_back/panoptic/scripts/test.py
@@ -1,13 +1,21 @@
 import asyncio
 
+import aiosqlite
+
 from panoptic.core.project.project import Project
 from panoptic.models import PropertyType
 
 
 async def run():
-    project = Project('/Users/david/panoptic-projects/script', [])
-    await project.start()
-    await project.db.add_property('lala', PropertyType.multi_tags, 'id')
-    await project.close()
+    async def query_data():
+        async with aiosqlite.connect('/Users/david/Downloads/panoptic__rkr.db') as db:
+            # Execute a SELECT query
+            async with db.execute('SELECT * FROM users') as cursor:
+                async for row in cursor:
+                    print(row)
+    # project = Project('/Users/david/panoptic-projects/script', [])
+    # await project.start()
+    # await project.db.add_property('lala', PropertyType.multi_tags, 'id')
+    # await project.close()
 
 asyncio.run(run())
