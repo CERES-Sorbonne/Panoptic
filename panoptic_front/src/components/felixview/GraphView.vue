017966121a6c8e13d9a8cf3edeb21a8699b3e0f3
Félix <someonefefe@gmail.com>
2024-03-18 11:24:21 +0100

    almost working graphview

--- panoptic_front/src/components/felixview/GraphView.vue
+++ panoptic_front/src/components/felixview/GraphView.vue
@@ -1,22 +1,79 @@
 <script setup lang="ts">
 import { CollectionManager } from '@/core/CollectionManager';
-import { GroupManager } from '@/core/GroupManager';
-import { defineProps, defineEmits } from 'vue'
+import { Group, GroupIterator, GroupManager } from '@/core/GroupManager';
+import { PropertyType } from '@/data/models';
+import { useProjectStore } from '@/data/projectStore';
+import { defineProps, defineEmits, computed, ref } from 'vue'
+import LineChart from './LineChart.vue'
 
+const project = useProjectStore()
 const props = defineProps<{
-    collection: CollectionManager
-    height: number
+  collection: CollectionManager
+  height: number
 }>()
 const emits = defineEmits([])
+const error = ref("")
+
+const series = computed(() => {
+  const res:{[key: string | number]: {[key: string] : any}} = {}
+
+  let properties = props.collection.groupManager.state.groupBy
+  if( properties.length > 2){
+      error.value = "Only max two levels of grouping are supported"
+      return
+  }
+  else if (properties.length === 1){
+    res['date'] = {name: 'date', data: []}
+  }
+  let it: GroupIterator = props.collection.groupManager.getGroupIterator()
+  while (it) {
+    const group: Group = it.group
+    if (group.id === "root") {
+      it = it.nextGroup()
+      continue
+    }
+    let propValue = group.meta.propertyValues[0]
+
+    if ('date' in res){
+      if(propValue.unit === undefined){
+        error.value = "First level of grouping needs to be a date property"
+        return
+      }
+      const date = propValue.value.getTime()
+      res['date'].data.push({x: date, y:group.images.length, images: group.images.slice(0, 10).map(i => i.url)})
+    }
+    else{
+      const date = propValue.value.getTime()
+      // const childValues = group.children.map(el => el.meta.propertyValues[0].value)
+      // const missingValues = allPropValues.filter(el => !childValues.includes(el))
+      for(let child of group.children){
+        const childValue = child.meta.propertyValues[0].value
+        if(res[childValue] === undefined){
+          res[childValue] = {data: [], name: project.data.tags[childValue].value}
+        }
+        res[child.meta.propertyValues[0].value].data.push({x: date, y: child.images.length, images: group.images.slice(0, 10).map(i => i.url)})
+      }
+      // for(let missing of missingValues){
+      //   res[missing].data.push([date, 0])
+      // }
+      group.children.forEach(() => it = it.nextGroup())
+    }
+    it = it.nextGroup()
+  }
+  error.value = ""
+  return Object.values(res)
+})
 </script>
 
 <template>
-  <div class="overflow-scroll" :style="{height: props.height+'px'}">
-    <div>{{ props.collection.filterManager.state }}</div>
+  <div class="" :style="{ height: props.height + 'px' }">
+    <!-- <div>{{ props.collection.filterManager.state }}</div>
     <div>{{ props.collection.sortManager.state }}</div>
     <div>{{ props.collection.groupManager.state }}</div>
+    <span v-for="item in data">{{item}}</span> -->
+    <line-chart :series="series" :height="(props.height - 50) + 'px'" v-if="error === ''"/>
+    <span v-else>{{error}}</span>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ panoptic_front/src/components/felixview/LineChart.vue
@@ -0,0 +1,62 @@
+<script setup>
+
+import { tryOnBeforeMount } from '@vueuse/core';
+import { defineProps, nextTick, onMounted, ref } from 'vue'
+
+const props = defineProps({
+    series: Array, // Spécifiez le type de la prop ici
+    height: String
+});
+
+
+// Options du graphique
+const chartOptions = {
+    xaxis: {
+        type: 'datetime',
+        // min: props.series[0].data[0][0],
+        // max: props.series[0].data[props.series[0].data.length - 1][0],
+        // tickAmount: 1000,
+    },
+    chart: {
+        stacked: false,
+        stackOnlyBar: false
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'smooth'
+    },
+    tooltip: {
+        shared: true,
+        custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+            const currentDataPoint = props.series[seriesIndex].data[dataPointIndex];
+
+            let imagesHTML = '<div style="display: grid; grid-template-columns: repeat(3, 1fr); grid-gap: 5px;">';
+            currentDataPoint.images.forEach((image, index) => {
+                if (index < 9) { // Ne traiter que les 9 premières images
+                    imagesHTML += `<div style="width: 75px; height: 75px;"><img src="${image}" style="width: 100%; height: 100%;" /></div>`;
+                }
+            });
+            imagesHTML += '</div>';
+
+            let tooltipHTML = `<span style="display: flex; align-items: center; justify-content: center;min-height:40px">${currentDataPoint.y} Images</span>`;
+            // tooltipHTML += "<hr />"
+            tooltipHTML += `<div>${imagesHTML}</div>`;
+
+            return tooltipHTML;
+        }
+    }
+
+};
+</script>
+
+<template>
+    <apexchart :height="props.height" type="area" :options="chartOptions" :series="props.series"></apexchart>
+</template>
+
+  
+<style scoped>
+/* Ajoutez vos styles CSS ici si nécessaire */
+</style>
+  
