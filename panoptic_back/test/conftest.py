4a09d02eac81d08ab75b4c7292230d0c3574a39e
david <godicked@gmail.com>
2024-09-21 14:30:31 +0200

    add read data test

--- panoptic_back/test/conftest.py
+++ panoptic_back/test/conftest.py
@@ -8,6 +8,7 @@ from panoptic.core.project.project import Project
 
 EMPTY_DB_PATH = "data/empty.db"
 INSTANCE_DB_PATH = "data/instance.db"
+DATA_DB_PATH = "data/data.db"
 
 
 @pytest.fixture
@@ -42,6 +43,25 @@ async def empty_project(request, tmp_path):
 @pytest.fixture
 async def instance_project(request, tmp_path):
     base_path = Path(request.fspath).parent / INSTANCE_DB_PATH
+
+    project_path = tmp_path / "tmp_project"
+    os.makedirs(project_path)
+    shutil.copy(base_path, project_path / 'panoptic.db')
+
+    project = Project(str(project_path), [])
+    await project.start()
+    # Yield the connection object to the test
+    yield project
+
+    # Close the connection after the test
+    await project.close()
+
+    # Cleanup is handled automatically by pytest when tmp_path is removed
+
+
+@pytest.fixture
+async def data_project(request, tmp_path):
+    base_path = Path(request.fspath).parent / DATA_DB_PATH
 
     project_path = tmp_path / "tmp_project"
     os.makedirs(project_path)
