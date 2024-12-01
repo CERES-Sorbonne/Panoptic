968c8e09a1dc8137f671b8ec76709e71e638f7bf
david <godicked@gmail.com>
2024-12-01 01:49:13 +0100

    fix import modal merge

--- panoptic_front/src/components/modals/ImportModal.vue
+++ panoptic_front/src/components/modals/ImportModal.vue
@@ -8,7 +8,7 @@ import { ref } from 'vue';
 import { ModalId } from '@/data/models';
 import { useDataStore } from '@/data/dataStore';
 import wTT from '@/components/tooltips/withToolTip.vue'
-import FusionModeDropdown from '../Dropdowns/FusionModeDropdown.vue';
+import FusionModeDropdown from '../dropdowns/FusionModeDropdown.vue';
 
 const panoptic = usePanopticStore()
 const data = useDataStore()
@@ -121,48 +121,19 @@ function clear() {
                         </td>
                     </tr>
                 </table>
-                <div v-if="properties.key == 'path'" >
+                <div v-if="properties.key == 'path'">
                     <div class="d-flex mt-3">
                         <div class="me-1">{{ $t('modals.import.fusion.title') }}</div>
                         <FusionModeDropdown :model-value="fusion" @update:model-value="e => fusion = e" />
                         <wTT :icon="true" message="modals.import.fusion.tooltip"></wTT>
                     </div>
                     <div class="d-flex mt-3">
-                        {{$t('modals.import.fusion.path')}}
-                        <input type="checkbox" :style="{ 'margin-left':'0.5rem' }" v-model="relative"/>
+                        {{ $t('modals.import.fusion.path') }}
+                        <input type="checkbox" :style="{ 'margin-left': '0.5rem' }" v-model="relative" />
                         <wTT :icon="true" message="modals.import.fusion.path_tooltip"></wTT>
                     </div>
                     <div>
-                        <div class="d-flex mt-2 flex-center w-20">
-                            <div v-if="!loading" class="bbb text-center w-100" @click="importFile">Import
-                            </div>
-                            <div v-if="loading" class="text-center w-100 border rounded">
-                                <div class="spinner-border spinner-border-sm"
-                                    style="position: relative; top: -1px" role="status">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    <tr v-if="properties.key == 'path'">
-                        <td colspan="3" class="pt-2">
-                            <div class="d-flex">
-                                <div class="me-1">Fusion Mode</div>
-                                <select v-model="fusion" :disabled="loading">
-                                    <option value="first">First</option>
-                                    <option value="last">Last</option>
-                                    <option value="new">New</option>
-                                    <option value="all">All</option>
-                                </select>
-                            </div>
-                        </td>
-                        <td colspan="2" class="pt-2">
-                            Relatif Path
-                            <input type="checkbox" v-model="relative" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="5" class="">
+                        <div class="d-flex mt-2 flex-center w-20" style="width: 300px;">
                             <div v-if="!missing" class="d-flex mt-2 flex-center w-100">
                                 <div v-if="!loading" class="bbb text-center w-100" @click="importFile">Import
                                 </div>
@@ -172,16 +143,17 @@ function clear() {
                                     </div>
                                 </div>
                             </div>
-                        </td>
-                    </tr>
+                        </div>
+                    </div>
+                </div>
 
-                </table>
 
                 <div v-if="missing">
                     <div class="text-warning mb-2">
                         {{ missing.length }} {{ $t('modals.import.not_found') }}
                     </div>
-                    <div v-if="!loading" style="width: 300px;" class="bbb text-center" @click="confirmImport">{{ $t('confirm') }}
+                    <div v-if="!loading" style="width: 300px;" class="bbb text-center" @click="confirmImport">{{
+                        $t('confirm') }}
                     </div>
                     <div v-if="loading" class="text-center w-100 border rounded">
                         <div class="spinner-border spinner-border-sm" style="position: relative; top: -1px"
@@ -191,7 +163,8 @@ function clear() {
                     <div class="mt-2 p-2"
                         style="max-height: 200px; overflow-y: auto; border: 1px solid var(--border-color);">
                         <div v-for="lines in missing">
-                            {{ $t('modals.import.row') }}: {{ lines[0] + 1 }} {{ $t('modals.import.key') }}: {{ lines[1] }}
+                            {{ $t('modals.import.row') }}: {{ lines[0] + 1 }} {{ $t('modals.import.key') }}: {{ lines[1]
+                            }}
                         </div>
                     </div>
                 </div>
