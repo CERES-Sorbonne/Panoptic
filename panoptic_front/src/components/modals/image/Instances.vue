99ddabed6c5c8b53796948098f76d2e8a20e8744
david <godicked@gmail.com>
2025-03-07 18:23:46 +0100

    fix image modal instance props

--- panoptic_front/src/components/modals/image/Instances.vue
+++ panoptic_front/src/components/modals/image/Instances.vue
@@ -3,9 +3,11 @@ import GridScroller from '@/components/scrollers/grid/GridScroller.vue';
 import { GroupManager } from '@/core/GroupManager';
 import { useDataStore } from '@/data/dataStore';
 import { Instance, PropertyID, PropertyMode } from '@/data/models';
+import { useTabStore } from '@/data/tabStore';
 import { computed, onMounted, watch } from 'vue';
 
 const data = useDataStore()
+const tabStore = useTabStore()
 
 const props = defineProps<{
     image: Instance
@@ -34,7 +36,7 @@ watch(() => props.image, update)
 <template>
     <div v-if="props.groupManager.hasResult()" class="m-0 p-0" style="overflow-x: scroll; overflow-y: hidden;"
         :style="{ width: props.width + 'px', height: props.height + 'px' }">
-        <GridScroller :show-images="false" :manager="props.groupManager" :height="props.height" :width="props.width - 15"
+        <GridScroller :tab="tabStore.getMainTab()" :show-images="false" :manager="props.groupManager" :height="props.height" :width="props.width - 15"
             :selected-properties="properties" />
     </div>
 </template>
