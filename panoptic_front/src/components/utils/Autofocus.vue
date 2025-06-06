e54b3e8dafdaad20a749fe4754fc3f46231f0fcb
david <godicked@gmail.com>
2025-06-06 10:24:13 +0200

    #381

---
+++ panoptic_front/src/components/utils/Autofocus.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, onMounted, nextTick } from 'vue'
+
+const elem = ref(null)
+
+onMounted(async () => {
+    await nextTick()
+    elem.value.focus()
+})
+</script>
+
+<template>
+    <div ref="elem" tabindex="1">
+        <slot>
+
+        </slot>
+    </div>
+</template>
+
+<style scoped></style>
