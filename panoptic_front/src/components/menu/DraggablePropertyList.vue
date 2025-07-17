9925396a496fa24eca72f847cbea5da14914adac
david <godicked@gmail.com>
2025-07-18 00:53:31 +0200

    udpate versions fix errors

---
+++ panoptic_front/src/components/menu/DraggablePropertyList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps<{
+    
+}>()
+const emits = defineEmits([])
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped>
+</style>
