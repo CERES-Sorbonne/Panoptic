1f37321f6de00cd47f98487634e68e691d5d922c
Tyrannas <someonefefe@gmail.com>
2025-11-17 09:43:30 +0100

    refactor error in db name

--- panoptic_back/panoptic/core/db/create.py
+++ panoptic_back/panoptic/core/db/create.py
@@ -192,7 +192,7 @@ def create_plugin_data():
 
 def create_project_params():
     query = """
-    CREATE TABLE _project (
+    CREATE TABLE project (
         key TEXT PRIMARY KEY,
         value JSON
     );
@@ -274,7 +274,7 @@ tables = {
     'vectors': create_vectors_table(),
     'ui_data': create_ui_data(),
     'plugin_data': create_plugin_data(),
-    '_project': create_project_params(),
+    'project': create_project_params(),
     'property_group': create_property_group_table(),
     'id_counter': create_id_counter_table()
 }
