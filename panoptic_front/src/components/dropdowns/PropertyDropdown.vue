8a1428b13ff05ec5427170234b115d3d44e428df
david <godicked@gmail.com>
2026-02-02 23:33:01 +0100

    no scrolling input

--- panoptic_front/src/components/dropdowns/PropertyDropdown.vue
+++ panoptic_front/src/components/dropdowns/PropertyDropdown.vue
@@ -21,7 +21,7 @@ const dropdownElem = ref(null)
         </template>
 
         <template #popup>
-            <div class="p-1" style="max-height: 400px; overflow-y: scroll;">
+            <div class="">
                 <PropertySelection @click="goNext()" @select="prop => {emits('select', prop); dropdownElem.hide()}" :ignore-ids="props.groupIds" />
             </div>
         </template>
