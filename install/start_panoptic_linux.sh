a0d2236a0d69f2ca2736cc06c7bdd5ff28455bcf
Orion Alié <someonefefe@gmail.com>
2026-05-05 15:40:22 +0200

    fix some bugs with uv
    Updated Python version for virtual environment and fixed command paths.

--- install/start_panoptic_linux.sh
+++ install/start_panoptic_linux.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
-
 set -e  # Arrêter le script en cas d'erreur
-
+PYTHON_VERSION=3.13
 # Ajouter .local/bin au PATH
 LOCAL_BIN="$HOME/.local/bin"
 export PATH="$LOCAL_BIN:$PATH"
-
 # Vérifier si uv est disponible, sinon l'installer
 if ! command -v uv &> /dev/null; then
     echo "Installation de uv..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
     export PATH="$LOCAL_BIN:$PATH"
 fi
-
 # Naviguer dans le dossier utilisateur
 cd "$HOME"
-
 # Créer le dossier panoptic s'il n'existe pas
 PANOPTIC_DIR="$HOME/panoptic"
 mkdir -p "$PANOPTIC_DIR"
 cd "$PANOPTIC_DIR"
-
 # Créer l'environnement virtuel avec Python 3.12 s'il n'existe pas
 if [ ! -d ".venv" ]; then
     echo "Création de l'environnement virtuel..."
-    uv venv --python 3.12
+	uv python install $PYTHON_VERSION
+    uv venv --python $PYTHON_VERSION
 fi
-
 # Installer la dernière version de pip
 echo "Installation de pip..."
 uv pip install pip
-
 # Vérifier si panoptic est installé, sinon l'installer
 if ! uv pip show panoptic &> /dev/null; then
     echo "Installation de panoptic..."
@@ -48,16 +42,24 @@ if ! uv pip show panoptic &> /dev/null; then
     fi
     
     # echo "Installation du plugin de similarité panopticml"
-    uv run panoptic plugins add vision
-fi
+    uv run .venv/bin/panoptic plugins add vision
 
+    read -p "Voulez-vous pré-télécharger le modèle CLIP (recommandé avant de venir à un atelier) ? (O/N) " download_clip
+    download_clip=$(echo "$download_clip" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$download_clip" = "o" ]; then
+        echo "Téléchargement du modèle CLIP..."
+        uvx hf download openai/clip-vit-base-patch32
+    fi
+fi
 # Vérifier si panoptic est obsolète
 echo "Vérification des mises à jour..."
+
 OUTDATED=$(uv pip list --outdated)
 
 if echo "$OUTDATED" | grep -q "panoptic"; then
     read -p "Mise à jour trouvée, voulez-vous l'installer ? (O/N) " user_input
-    user_input=$(echo "$user_input" | tr '[:upper:]' '[:lower:]')
+    user_input=$(echo "$user_input" | tr '[:upper:]' '[:lower]')
     
     if [ "$user_input" = "o" ]; then
         echo "Mise à jour de panoptic..."
@@ -66,6 +68,5 @@ if echo "$OUTDATED" | grep -q "panoptic"; then
 else
     echo "La dernière version de panoptic est déjà installée."
 fi
-
 echo "Lancement de panoptic..."
-uv run panoptic
+uv run .venv/bin/panoptic
