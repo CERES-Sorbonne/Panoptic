b505517eef04eb38e58472c3cb4caed9c3e9e79b
david.godicke <godicked@gmail.com>
2026-06-05 03:56:53 +0200

    wip chart

--- panoptic_front/src/components/graphview/GraphView.vue
+++ panoptic_front/src/components/graphview/GraphView.vue
@@ -2,11 +2,13 @@
 import { CollectionManager } from '@/core/CollectionManager';
 import { Group, GroupIterator } from '@/core/GroupManager';
 import { useDataStore } from '@/data/dataStore';
+import { useColumnStore } from '@/data/columnStore';
 import { PropertyType } from '@/data/models';
 import { ref } from 'vue'
 import LineChart from './LineChart.vue'
 
 const dataStore = useDataStore()
+const columnStore = useColumnStore()
 const props = defineProps<{
     collection: CollectionManager
     height: number
@@ -14,7 +16,7 @@ const props = defineProps<{
 const emits = defineEmits([])
 const error = ref("")
 
-const chartData = ref(computeSeries())
+const chartData = ref<{ series: any[]; xValues: number[]; dataType: PropertyType } | null>(null)
 
 function getAllPropValues() {
     const allPropValues = new Set();
@@ -37,20 +39,23 @@ function getAllPropValues() {
 }
 
 function computeSeries() {
+    if (!props.collection.groupManager.hasResult()) return null
+
     const res: { [key: string | number]: { [key: string]: any } } = {}
     let allPropValues;
     let properties = props.collection.groupManager.state.groupBy
 
     if (properties.length === 0) {
         error.value = "Choose at least one date or numeric value to group the images by"
-        return
+        return null
     }
     const firstProp = dataStore.properties[properties[0]]
+    if (!firstProp) return null
     const firstPropType = firstProp.type
 
     if (properties.length > 2) {
         error.value = "Only max two levels of grouping are supported"
-        return
+        return null
     }
     else if (properties.length === 1) {
         res[firstProp.name] = { name: firstProp.name, data: [] }
@@ -60,13 +65,20 @@ function computeSeries() {
         allPropValues = Array.from(getAllPropValues())
         if(allPropValues.length > 20){
             error.value = "Too many curves to draw, select a subgrouping with less than 20 possible values"
-            return
+            return null
         }
     }
     if (firstPropType !== PropertyType.number && firstPropType !== PropertyType.date) {
         error.value = "First level of grouping needs to be a date or a numeric property"
-        return
+        return null
     }
+
+    const sha1PropId = columnStore.systemProps.SHA1
+    const getImgUrls = (slots: number[]) => {
+        if (!sha1PropId) return []
+        return slots.slice(0, 20).map(s => dataStore.baseImgUrl + columnStore.readSlot(sha1PropId, s) + '?size=256')
+    }
+
     let it: GroupIterator = props.collection.groupManager.getGroupIterator()
     const xValues = []
     while (it) {
@@ -80,7 +92,8 @@ function computeSeries() {
         const xValue = firstPropType === PropertyType.date ? new Date(propValue.value).getTime() : propValue.value
         xValues.push(xValue)
         if (firstProp.name in res) {
-            res[firstProp.name].data.push({ x: xValue, y: group.images.length, images: group.images.slice(0, 20).map(i => i.imageUrl + '?size=256') })
+            const imgUrls = getImgUrls(group.slots)
+            res[firstProp.name].data.push({ x: xValue, y: group.slots.length, images: imgUrls })
         }
         else {
             const childValues = group.children.map(el => el.meta.propertyValues[0].value)
@@ -95,7 +108,8 @@ function computeSeries() {
                     }
                     res[childValue] = { data: [], name: value }
                 }
-                res[child.meta.propertyValues[0].value].data.push({ x: xValue, y: child.images.length, images: child.images.slice(0, 20).map(i => i.imageUrl + '?size=256') })
+                const childImgUrls = getImgUrls(child.slots)
+                res[child.meta.propertyValues[0].value].data.push({ x: xValue, y: child.slots.length, images: childImgUrls })
             }
             // console.log(xValue, missingValues)
             for (let missing of missingValues) {
@@ -121,12 +135,13 @@ function computeSeries() {
         dataType: firstPropType
     }
 }
+
 props.collection.groupManager.onResultChange.addListener(() => chartData.value = computeSeries())
 </script>
 
 <template>
     <div class="" :style="{ height: props.height + 'px' }">
-        <line-chart :chartData="chartData" :height="(props.height - 50) + 'px'" v-if="error === ''" />
+        <line-chart :chartData="chartData" :height="(props.height - 50) + 'px'" v-if="error === '' && chartData" />
         <span v-else>{{ error }}</span>
     </div>
 </template>
